d2bf3b46a45477b2a18447f078be83074fc399ac
hajos8 <rabb.andras@gmail.com>
2025-09-24 11:42:50 +0200

    1-2-3-4 feladat

---
+++ untitled/src/main/java/org/example/Jackie.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Jackie {
+    ArrayList<Integer[]> stats = new ArrayList<>();
+
+    public ArrayList<Integer[]> getStats() {
+        return stats;
+    }
+    public void setStats(ArrayList<Integer[]> stats) {
+        this.stats = stats;
+    }
+
+    public void readStats(String path) {
+        try{
+            Scanner scanner = new Scanner(new File(path));
+
+            scanner.nextLine(); // skip header
+
+            ArrayList<Integer[]> tempStats = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                String[] parts = line.split("\t");
+                Integer[] intParts = new Integer[parts.length];
+
+                for (int i = 0; i < parts.length; i++) {
+                    intParts[i] = Integer.parseInt(parts[i]);
+                }
+
+                tempStats = getStats();
+                tempStats.add(intParts);
+                setStats(tempStats);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+    }
+
+    public int legtobbVerseny(){
+        int max = 0;
+        int index = 0;
+
+        for(int i = 0; i < stats.size(); i++){
+            if(stats.get(i)[1] > max){
+                max = stats.get(i)[1];
+                index = i;
+            }
+        }
+
+        return stats.get(index)[0];
+    }
+
+    publi
+}
--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Jackie handler = new Jackie();
+
+        //2. feladat
+        handler.readStats("jackie.txt");
+
+//        for(Integer[] stats : handler.getStats()){
+//            for(Integer stat : stats){
+//                System.out.print(stat + " ");
+//            }
+//            System.out.println();
+//        }
+
+        //year	races	wins	podiums	poles	fastests
+
+        //3. feladat
+
+        System.out.println("3. feladat: " + handler.getStats().size());
+
+        //4. feladat
+
+        System.out.println("4. feladat: " + handler.legtobbVerseny());
+
+
     }
 }
